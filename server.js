67162924e7ebeae268d892d661a4906f793a4b12
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-10-26 15:49:52 -0300

    REQ1 - Adicionando Conteudo do Requisito - [DONE]

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
-// Faça seu código aqui
+const express = require('express');
+
+const app = express();
+const http = require('http').createServer(app);
+
+// const { Server } = require('socket.io');
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'https://localhost:3000',
+    method: ['GET', 'POST'],
+  },
+});
+
+app.use(express.json());
+// const io = Server(server);
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    const { chatMessage, nickname } = msg;
+    const today = new Date().toLocaleString().replace(/\//g, '-');
+    io.emit('message', `${today} - ${nickname} - ${chatMessage}`);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+// Agradecimentos A Lucas Martins Turma 10 - Tribo B, pela orientação na estruturação do requisito 1.
